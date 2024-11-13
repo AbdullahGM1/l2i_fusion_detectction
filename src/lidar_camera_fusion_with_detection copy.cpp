471d1dd79ea8d16f268269bf884debfa2e686b6b
Abdullah <141170550+AbdullahGM1@users.noreply.github.com>
2024-11-13 14:13:51 +0300

    Update lidar_camera_fusion_with_detection copy.cpp

--- src/lidar_camera_fusion_with_detection copy.cpp
+++ src/lidar_camera_fusion_with_detection copy.cpp
@@ -50,7 +50,8 @@ public:
             "/yolo/tracking", rclcpp::QoS(10), std::bind(&LidarCameraFusionNode::detectionCallback, this, std::placeholders::_1));
 
         image_pub_ = this->create_publisher<sensor_msgs::msg::Image>("/image_lidar", 10);
-        pose_pub_ = this->create_publisher<geometry_msgs::msg::PoseArray>("/detected_object_position", 10);
+        pose_pub_ = this->create_publisher<geometry_msgs::msg::PoseArray>("/detected_objectpose", 10);
+        combined_pointcloud_pub_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("/detected_object_pointcloud", 10);
     }
 
 private:
@@ -60,6 +61,7 @@ private:
     rclcpp::Subscription<yolov8_msgs::msg::DetectionArray>::SharedPtr detection_sub_;
     rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr image_pub_;
     rclcpp::Publisher<geometry_msgs::msg::PoseArray>::SharedPtr pose_pub_;
+    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr combined_pointcloud_pub_;
 
     tf2_ros::Buffer tf_buffer_;
     tf2_ros::TransformListener tf_listener_;
@@ -136,11 +138,10 @@ private:
         pass.setFilterLimits(min_depth_, max_depth_);
         pass.filter(*cloud_filtered);
 
-        cv::Mat image_with_points = current_image_.clone();
-        std::map<int, std::vector<pcl::PointXYZ>> bbox_points;
+        pcl::PointCloud<pcl::PointXYZ>::Ptr combined_cloud(new pcl::PointCloud<pcl::PointXYZ>);
         geometry_msgs::msg::PoseArray pose_array;
         pose_array.header.stamp = rclcpp::Clock().now();
-        pose_array.header.frame_id = "lidar_frame";  // Adjust frame ID as necessary
+        pose_array.header.frame_id = msg->header.frame_id;
 
         for (const auto &point : cloud_filtered->points) {
             Eigen::Vector4f homogeneous_point(point.x, point.y, point.z, 1.0);
@@ -150,8 +151,7 @@ private:
                 double u = (fx_ * transformed_point[0] / transformed_point[2]) + cx_;
                 double v = (fy_ * transformed_point[1] / transformed_point[2]) + cy_;
 
-                for (int i = 0; i < current_detections_.size(); i++) {
-                    auto &detection = current_detections_[i];
+                for (const auto &detection : current_detections_) {
                     auto &bbox = detection.bbox;
                     double x_min = bbox.center.position.x - bbox.size.x / 2.0;
                     double y_min = bbox.center.position.y - bbox.size.y / 2.0;
@@ -159,43 +159,28 @@ private:
                     double y_max = bbox.center.position.y + bbox.size.y / 2.0;
 
                     if (u >= x_min && u <= x_max && v >= y_min && v <= y_max) {
-                        bbox_points[i].push_back(point);
-                        cv::circle(image_with_points, cv::Point(u, v), 3, cv::Scalar(0, 255, 0), -1);
+                        combined_cloud->push_back(point);
+                        geometry_msgs::msg::Pose pose;
+                        pose.position.x = point.x;
+                        pose.position.y = point.y;
+                        pose.position.z = point.z;
+                        pose.orientation.w = 1.0; // No rotation information
+                        pose_array.poses.push_back(pose);
                         break;
                     }
                 }
             }
         }
 
-        for (auto &[id, points] : bbox_points) {
-            if (!points.empty()) {
-                double sum_x = 0, sum_y = 0, sum_z = 0;
-                for (auto &p : points) {
-                    sum_x += p.x;
-                    sum_y += p.y;
-                    sum_z += p.z;
-                }
-                double avg_x = sum_x / points.size();
-                double avg_y = sum_y / points.size();
-                double avg_z = sum_z / points.size();
-
-                geometry_msgs::msg::Pose pose;
-                pose.position.x = avg_x;
-                pose.position.y = avg_y;
-                pose.position.z = avg_z;
-                pose.orientation.w = 1.0; // No rotation information
-                pose_array.poses.push_back(pose);
-
-                RCLCPP_INFO(this->get_logger(), "BB%d Average pose: X = %f, Y = %f, Z = %f", id+1, avg_x, avg_y, avg_z);
-            }
+        if (!combined_cloud->empty()) {
+            sensor_msgs::msg::PointCloud2 output_cloud;
+            pcl::toROSMsg(*combined_cloud, output_cloud);
+            output_cloud.header.stamp = msg->header.stamp;
+            output_cloud.header.frame_id = msg->header.frame_id;
+            combined_pointcloud_pub_->publish(output_cloud);
         }
 
-        // Publish the pose array
         pose_pub_->publish(pose_array);
-
-        // Publish the image with points
-        auto output_image = cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8", image_with_points).toImageMsg();
-        image_pub_->publish(*output_image);
     }
 };
 
