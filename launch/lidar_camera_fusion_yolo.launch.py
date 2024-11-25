75a9687112d5449a4b8a856e3e56999f5f8300ca
Abdullah <141170550+AbdullahGM1@users.noreply.github.com>
2024-11-25 10:50:11 +0300

    launch file

--- launch/lidar_camera_fusion_yolo.launch.py
+++ launch/lidar_camera_fusion_yolo.launch.py
@@ -11,13 +11,16 @@ def generate_launch_description():
         executable='lidar_camera_fusion_with_detection',
         name='lidar_camera_fusion_node',
         parameters=[
-            {'min_depth': 0.2, 'max_depth': 10.0}
+            {'min_depth': 0.2, 'max_depth': 10.0, # Setup your min and max depth range, where (x-axis) is the depth
+             'lidar_frame': 'x500_mono_1/lidar_link/gpu_lidar',  # Default source frame
+             'camera_frame': 'interceptor/gimbal_camera'}  # Default target frame
         ],
         remappings=[
-            ('/scan/points', '/scan/points'),
-            ('/interceptor/gimbal_camera_info', '/interceptor/gimbal_camera_info'),
-            ('/interceptor/gimbal_camera', '/interceptor/gimbal_camera'),
-            ('/yolo/tracking', '/yolo/tracking')
+            # Replace with actual topic names
+            ('/scan/points', '/scan/points'), # The lidar point cloud topic
+            ('/interceptor/gimbal_camera_info', '/interceptor/gimbal_camera_info'),# The camera info topic
+            ('/interceptor/gimbal_camera', '/interceptor/gimbal_camera'), # The camera image topic 
+            ('/yolo/tracking', '/yolo/tracking') # The YOLO BB tracking topic
         ]
     )
 
@@ -40,4 +43,3 @@ def generate_launch_description():
         lidar_camera_fusion_node,
         yolov8_launch
     ])
-
